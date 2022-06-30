fb95628c28bc747dccfc2f0408390bd684b5094b
sagit <601096721@qq.com>
2022-06-30 16:32:47 +0800

    update 4.x

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ if [ ! -f /etc/mosdns/config.yaml ]; then
     cp -u /geosite.dat /etc/mosdns/geosite.dat
     cp -u /geoip.dat /etc/mosdns/geoip.dat
 fi
-/usr/bin/mosdns -dir /etc/mosdns
+/usr/bin/mosdns start --dir /etc/mosdns
