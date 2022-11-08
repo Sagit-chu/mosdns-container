ad1b56881fc17bd9106289237c58119474373f2f
mjyhj <22577418+mjyhj@users.noreply.github.com>
2022-11-09 02:51:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ ! -f /etc/mosdns/config.yaml ]; then
-	mkdir -p /etc/mosdns/
-	cp -u /config.yaml /etc/mosdns/config.yaml
-    	cp -u /geosite.dat /etc/mosdns/geosite.dat
-    	cp -u /geoip.dat /etc/mosdns/geoip.dat
+mkdir -p /etc/mosdns/
+cp -u /config.yaml /etc/mosdns/config.yaml
 fi
+cp -u /geosite.dat /etc/mosdns/geosite.dat
+cp -u /geoip.dat /etc/mosdns/geoip.dat
 /usr/bin/mosdns start --dir /etc/mosdns
