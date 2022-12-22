c8d243247f2c7b2d10e50d92e5220389d763d136
db-one <20243226+db-one@users.noreply.github.com>
2022-12-22 11:02:16 +0800

    复制hosts到配置文件目录

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ if [ ! -f /etc/mosdns/config.yaml ]; then
 	mkdir -p /etc/mosdns/
 	cp -u /config.yaml /etc/mosdns/config.yaml
 fi
+if [ ! -f /etc/mosdns/hosts ]; then
+	cp -u /hosts /etc/mosdns/hosts
+fi
 cp -u /geosite.dat /etc/mosdns/geosite.dat
 cp -u /geoip.dat /etc/mosdns/geoip.dat
 /usr/bin/mosdns start --dir /etc/mosdns
